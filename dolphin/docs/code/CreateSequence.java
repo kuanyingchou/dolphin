fd366eaeabc140770ebde577180c622b17ff2e5b
KUAN-YING CHOU <kuanyingchou@gmail.com>
2012-10-30 20:58:15 +0800

    simplify sample code

--- dolphin/docs/code/CreateSequence.java
+++ dolphin/docs/code/CreateSequence.java
@@ -10,61 +10,30 @@ import javax.sound.midi.Track;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.InvalidMidiDataException;
 
-public class CreateSequence {
-   private static final int  VELOCITY = 64;
+public class TestSequence {
 
    public static void main(String[] args) 
          throws MidiUnavailableException, InvalidMidiDataException {
-      Sequence sequence=null;
-      sequence = new Sequence(Sequence.PPQ, 1); //what is PPQ?
-
-      final Track track = sequence.createTrack();
-
-      track.add(createNoteOnEvent(60, 0));
-      track.add(createNoteOffEvent(60, 2));
-
-      track.add(createNoteOnEvent(62, 2));
-      track.add(createNoteOffEvent(62, 4));
-
-      track.add(createNoteOnEvent(64, 4));
-      track.add(createNoteOffEvent(64, 6));
-
-      track.add(createNoteOnEvent(65, 6));
-      track.add(createNoteOffEvent(65, 8));
-
+      final Sequence sequence=create(); 
       SimpleSequencePlayer.play(sequence);
    }
 
-   private static MidiEvent createNoteOnEvent(int key, long tick) 
-         throws InvalidMidiDataException {
-      return createNoteEvent(ShortMessage.NOTE_ON, key, VELOCITY, tick);
+   public static Sequence create() throws InvalidMidiDataException {
+      final Sequence sequence=new Sequence(Sequence.PPQ, 1);
+      final Track track = sequence.createTrack();
+      track.add(createNoteEvent(0, ShortMessage.NOTE_ON, 0, 60, 64));
+      track.add(createNoteEvent(2, ShortMessage.NOTE_ON, 0, 60, 0));
+      return sequence;
    }
 
-
-
-   private static MidiEvent createNoteOffEvent(int key, long tick)
+   public static MidiEvent createNoteEvent(
+         long tick, int command, int channel, int key, int velocity) 
          throws InvalidMidiDataException {
-      return createNoteEvent(ShortMessage.NOTE_OFF,
-                 key,
-                 0,
-                 tick);
-   }
-
-   private static MidiEvent createNoteEvent(
-      int command,
-      int key,
-      int velocity,
-      long tick) throws InvalidMidiDataException
-   {
-      ShortMessage  message = new ShortMessage();
-      message.setMessage(command,
-               0, // always on channel 1
-               key,
-               velocity);
-      MidiEvent event = new MidiEvent(message, tick);
+      final ShortMessage  message = new ShortMessage();
+      message.setMessage(command, channel, key, velocity);
+      final MidiEvent event = new MidiEvent(message, tick);
       return event;
-  }
-
+   }
 }
 
 
